70a11d06cba33ea57d5ebad85ad039121306fee6
FY Kuo <teresa99339@gmail.com>
2022-07-24 13:44:21 +0800

    新增base檔案 將需要用的sql語法 及 試題上的文字建立成物件導向

--- back.php
+++ back.php
@@ -1,5 +1,6 @@
 ﻿<?php
 $do = $_GET['do'] ?? 'title';
+include('./api/base.php');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0068)?do=admin&redo=title -->
